e523d9d3fc8ab3a71790a7db6a93375e8fe9c664
omarurregodev <omarurrego07@gmail.com>
2022-05-15 19:45:25 -0500

    agregue herramientas adicionales, hice fetch para datos climatologicos y mas cryptos. Agregue la confirmacion del formato de correo electronico. Inclui la libreria sweetalert en lugar de los alert default. elimine bugs y adicione confirmaciones

--- main.js
+++ main.js
@@ -11,18 +11,86 @@ let btnModifyUserModal = document.getElementById("btnModifyUserModal");
 
 //let graphicArray = [];
 
-// API PARA MOSTRAR EL PRECIO DEL BTC EN TIEMPO REAL
+// API PARA MOSTRAR EL PRECIO DE LAS CRYPTOS PRINCIPALES
 
 $(function () {
-  fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
-    .then((response) => response.json())
-    .then((data) => {
-      BTC_price = data.bpi.USD.rate;
-      console.log("esto es data: " + BTC_price);
-      document.getElementById("BTC_current_price").innerHTML =
-        "<span>US$ </span>" + BTC_price;
-    });
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Host': 'coingecko.p.rapidapi.com',
+			'X-RapidAPI-Key': 'f53d130201msh51c2bcdc7b28436p126b7bjsne4b54f265fdb'
+		}
+	};
+	
+	fetch('https://coingecko.p.rapidapi.com/coins/bitcoin?localization=true&tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=false', options)
+	.then(response => response.json())
+	.then(data => {
+		//console.log(data);
+		BTC_price = data.market_data.current_price.usd;
+		BTC_price_change_24h = data.market_data.price_change_24h;
+		if (BTC_price_change_24h > 0) {
+			document.getElementById("BTC_current_price").innerHTML = "<span style='color:green'> <i class='fa-solid fa-sort-up'></i> US$ "+ BTC_price +"</span>";
+		} else {
+			document.getElementById("BTC_current_price").innerHTML = "<span style='color:red'> <i class='fa-solid fa-sort-down'></i> US$ "+ BTC_price +"</span>";
+		}
+	});
+	fetch('https://coingecko.p.rapidapi.com/coins/ethereum?localization=true&tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=false', options)
+	.then(response => response.json())
+	.then(data => {
+		//console.log(data);
+		ETH_price = data.market_data.current_price.usd;
+		ETH_price_change_24h = data.market_data.price_change_24h;
+		if (ETH_price_change_24h > 0) {
+			document.getElementById("ETH_current_price").innerHTML = "<span style='color:green'> <i class='fa-solid fa-sort-up'></i> US$ "+ ETH_price +"</span>";
+		} else {
+			document.getElementById("ETH_current_price").innerHTML = "<span style='color:red'> <i class='fa-solid fa-sort-down'></i> US$ "+ ETH_price +"</span>";
+		}
+	});
+	fetch('https://coingecko.p.rapidapi.com/coins/solana?localization=true&tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=false', options)
+	.then(response => response.json())
+	.then(data => {
+		//console.log(data);
+		SOL_price = data.market_data.current_price.usd;
+		SOL_price_change_24h = data.market_data.price_change_24h;
+		if (SOL_price_change_24h > 0) {
+			document.getElementById("SOL_current_price").innerHTML = "<span style='color:green'> <i class='fa-solid fa-sort-up'></i> US$ "+ SOL_price +"</span>";
+		} else {
+			document.getElementById("SOL_current_price").innerHTML = "<span style='color:red'> <i class='fa-solid fa-sort-down'></i> US$ "+ SOL_price +"</span>";
+		}
+	});
 });
+
+// API PARA MOSTRAR DATOS DEL CLIMA EN BOGOTA D.C
+
+$(function () {
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com',
+			'X-RapidAPI-Key': 'f53d130201msh51c2bcdc7b28436p126b7bjsne4b54f265fdb'
+		}
+	};
+	
+	fetch('https://open-weather13.p.rapidapi.com/city/bogota', options)
+	.then(response => response.json())
+	.then(data => {
+		temp_data_f = data.main.temp;
+		//console.log(temp_data_f);
+		temp_data_c = ((temp_data_f - 32) * 5) / 9;
+		//console.log(Math.round(temp_data_c));
+		document.getElementById("temp_data").innerHTML = "<span>"+ Math.round(temp_data_c) +" °C</span>";
+	});
+	
+	fetch('https://open-weather13.p.rapidapi.com/city/bogota', options)
+	.then(response => response.json())
+	.then(data => {
+		//console.log(data);
+		humidity_data = data.main.humidity;
+		document.getElementById("humidity_data").innerHTML = "<span>"+ humidity_data +" %</span>";
+	});
+	
+});
+
 
 // AGREGO LOS LISTENERS DE LOS BOTONES
 $(btnAdd).on("click", function () {
@@ -84,54 +152,60 @@ function onloadLocalstorage() {
 }
 
 function newUser() {
-  if (
-    document.getElementById("name").value == "" ||
-    document.getElementById("age").value == "" ||
-    document.getElementById("phone").value == "" ||
-    document.getElementById("email").value == ""
-  ) {
-    alert("Todos los campos deben de estar llenos.");
-  } else if (userArray.length == 0) {
-    let user = {
-      name: document.getElementById("name").value,
-      age: document.getElementById("age").value,
-      phone: document.getElementById("phone").value,
-      email: document.getElementById("email").value,
-    };
-    userArray.push(user);
-    //graficar(userArray);
+	// EN ESTE CONDICIONAL EVALUO SI EL EMAIL INGRESADO ES VALIDO O NO... ESTOY CONFIRMANDO EL FORMATO DE ESTE
+	re = /^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+	if (!re.exec(document.getElementById("email").value)) {
+		swal("Error!","Se ha ingresado un email no valido.","error");
+	} else if (
+		document.getElementById("name").value == "" ||
+		document.getElementById("age").value == "" ||
+		document.getElementById("phone").value == "" ||
+		document.getElementById("email").value == "" 
+	) {
+		swal("Error!","Todos los campos deben de estar llenos.","error");
+	} else if (userArray.length == 0) {
+		swal("Hecho!","El contacto se ha registrado con exito!","success");
+		let user = {
+		name: document.getElementById("name").value,
+		age: document.getElementById("age").value,
+		phone: document.getElementById("phone").value,
+		email: document.getElementById("email").value,
+		};
+		userArray.push(user);
+		//graficar(userArray);
 
-    // AQUI TENGO QUE EXPORTAR LA INFO AL LOCAL STORAGE - Y DEBO DE ENVIAR ESE ARRAY A GRAFICAR
-    localStorage.setItem("usuarios", JSON.stringify(userArray));
-    //userArray = localStorage.getItem('usuarios');  ESTO ES INNECESARIO
-    //userArray = JSON.parse(userArray);  ESTO ES INNECESARIO
+		// AQUI TENGO QUE EXPORTAR LA INFO AL LOCAL STORAGE - Y DEBO DE ENVIAR ESE ARRAY A GRAFICAR
+		localStorage.setItem("usuarios", JSON.stringify(userArray));
+		//userArray = localStorage.getItem('usuarios');  ESTO ES INNECESARIO
+		//userArray = JSON.parse(userArray);  ESTO ES INNECESARIO
 
-    graficar(userArray);
-    //console.log(userArray);
-  } else {
-    nameInput = document.getElementById("name").value;
-    filtro = userArray.filter((user) => user.name == nameInput); //AQUI ESTOY HACIENDO EL FILTRO PARA VER SI EL USUARIO QUE SE ESTA DIGITANDO YA ESTA CREADO
-    if (filtro.length == 0) {
-      let user = {
-        name: document.getElementById("name").value,
-        age: document.getElementById("age").value,
-        phone: document.getElementById("phone").value,
-        email: document.getElementById("email").value,
-      };
-      userArray.push(user);
-      //graficar(userArray);
+		graficar(userArray);
+		//console.log(userArray);
+	} else {
+		nameInput = document.getElementById("name").value;
+		filtro = userArray.filter((user) => user.name == nameInput); //AQUI ESTOY HACIENDO EL FILTRO PARA VER SI EL USUARIO QUE SE ESTA DIGITANDO YA ESTA CREADO
+		if (filtro.length == 0) {
+			swal("Hecho!","El contacto se ha registrado con exito!","success");
+			let user = {
+				name: document.getElementById("name").value,
+				age: document.getElementById("age").value,
+				phone: document.getElementById("phone").value,
+				email: document.getElementById("email").value,
+			};
+			userArray.push(user);
+			//graficar(userArray);
 
-      // AQUI TENGO QUE EXPORTAR LA INFO AL LOCAL STORAGE - Y DEBO DE ENVIAR ESE ARRAY A GRAFICAR
-      localStorage.setItem("usuarios", JSON.stringify(userArray));
-      //userArray = localStorage.getItem('usuarios');  ESTO ES INNECESARIO
-      //userArray = JSON.parse(userArray);  ESTO ES INNECESARIO
+			// AQUI TENGO QUE EXPORTAR LA INFO AL LOCAL STORAGE - Y DEBO DE ENVIAR ESE ARRAY A GRAFICAR
+			localStorage.setItem("usuarios", JSON.stringify(userArray));
+			//userArray = localStorage.getItem('usuarios');  ESTO ES INNECESARIO
+			//userArray = JSON.parse(userArray);  ESTO ES INNECESARIO
 
-      graficar(userArray);
-      //console.log(userArray);
-    } else {
-      alert("El usuario ya se encuentra creado");
-    }
-  }
+			graficar(userArray);
+			//console.log(userArray);
+		} else {
+			swal("Oops!","El contacto ya se encuentra creado.","warning");
+		}
+	}
 }
 
 function graficar(usuarios) {
@@ -143,19 +217,19 @@ function graficar(usuarios) {
   for (let i = 0; i < usuarios.length; i++) {
     perfil =
       perfil +
-      "<div class='card mx-2 p-2' style='width: 18rem;'> <div class='card-body'> <h5 class='card-title'>" +
+      "<div class='card mx-2 col-md-4 col-lg-4 col-sm-12' style='width: 18rem;'><img src='./img/avatar_profile.png' class='card-img-top rounded-circle' alt='Imagen de contacto'> <div class='card-body'> <h5 class='card-title'>" +
       usuarios[i].name +
-      "</h5> <p class='card-text'>" +
+      "</h5> <p class='card-text'>Edad: " +
       usuarios[i].age +
-      "</p> <p class='card-text'>" +
+      "</p> <p class='card-text'>Telefono: " +
       usuarios[i].phone +
-      "</p> <p class='card-text'>" +
+      "</p> <p class='card-text'>Email: " +
       usuarios[i].email +
-      "</p> </div> <div class='card-body'> <button class='btn m-2 btn-danger' onClick='deleteUser(" +
+      "</p> </div> <div class='card-body'> <button class='btn btn-danger' onClick='deleteUser(" +
       i +
-      ");' data-bs-toggle='modal' data-bs-target='#deleteModal'><i class='fa-solid fa-user-xmark'></i>  Eliminar</button></button><button class='btn m-2 btn-primary' onClick='modifyUser(" +
+      ");' data-bs-toggle='modal' data-bs-target='#deleteModal'><i class='fa-solid fa-user-xmark'></i>  Eliminar</button><button class='btn ms-1 btn-primary' onClick='modifyUser(" +
       i +
-      ");' data-bs-toggle='modal' data-bs-target='#modifyModal'><i class='fa-solid fa-user-xmark'></i>  Modificar</button></button></div> </div>";
+      ");' data-bs-toggle='modal' data-bs-target='#modifyModal'><i class='fa-solid fa-user-xmark'></i>  Modificar</button></div> </div>";
   }
   //Renderizo todo apenas acabe
   html_perfil.innerHTML = perfil;
@@ -205,6 +279,7 @@ function deleteUserConfirm() {
     localStorage.setItem("usuarios", JSON.stringify(userArray)); //MODIFICO EL ARRAY DE CARDS
     localStorage.setItem("id_eliminar", -1); // TENGO QUE SETEAR NUEVAMENTE EL VALOR DE ID_ELIMINAR EN -1 PARA QUE LA VALIDACION SEA CORRECTA
     graficar(userArray);
+	swal("Hecho!","El contacto se ha eliminado exitosamente.","success");
   }
 }
 
@@ -226,15 +301,20 @@ function modifyUser(id) {
 }
 
 function modifyUserConfirm() {
+	id = localStorage.getItem("id_modificar"); // OBTENGO EL ID DEL SOTRAGE PARA MODIFICAR CONTACTO
+	
 	if (
 		document.getElementById("nameModify").value == "" ||
 		document.getElementById("ageModify").value == "" ||
 		document.getElementById("phoneModify").value == "" ||
 		document.getElementById("emailModify").value == ""
 	  ) {
-		alert("Todos los campos deben de estar llenos.");
-	  } else {
-		id = localStorage.getItem("id_modificar"); // OBTENGO EL ID DEL SOTRAGE PARA MODIFICAR CONTACTO
+		swal("Error!","Todos los campos deben de estar llenos.","error");
+	  } else if(userArray[id].name != document.getElementById("nameModify").value ||
+	  userArray[id].age != document.getElementById("ageModify").value ||
+	  userArray[id].phone != document.getElementById("phoneModify").value ||
+	  userArray[id].email != document.getElementById("emailModify").value
+	  ) {
 		userArray[id].name = document.getElementById("nameModify").value;
 		userArray[id].age = document.getElementById("ageModify").value;
 		userArray[id].phone = document.getElementById("phoneModify").value;
@@ -243,6 +323,8 @@ function modifyUserConfirm() {
 		localStorage.setItem("usuarios", JSON.stringify(userArray)); //MODIFICO EL ARRAY DE CARDS
 		localStorage.setItem("id_modificar", -1); // TENGO QUE SETEAR NUEVAMENTE EL VALOR DE ID_MODIFICAR EN -1 PARA QUE LA VALIDACION SEA CORRECTA
 		graficar(userArray);
-		alert('Contacto modificado con exito!');
+		swal("Hecho!","Contacto modificado con exito!","success");
+	  } else {
+		swal("Oops!","Los datos son iguales a los existentes.","warning");
 	  }	
 }
